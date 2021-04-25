401da3e0378d44a88b63365cd76f9edfde958a24
Matheus Marques <marquesm91@gmail.com>
2021-04-25 15:56:26 -0300

    feat: add basic actions

---
+++ lib/flutter_test_behavior.dart
@@ -0,0 +1,46 @@
+library flutter_test_behavior;
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class UserEvents {
+  UserEvents(this.tester, this.key);
+
+  final WidgetTester tester;
+  final Key key;
+  String _text = '';
+
+  Finder get _finder => find.byKey(key);
+
+  String get text => _text;
+
+  T widget<T extends Widget>() => tester.widget<T>(_finder);
+
+  Future<void> type(String text) async {
+    await tap();
+
+    List<int> runes = text.runes.toList();
+
+    for (var i = 0; i < runes.length; i += 1) {
+      final char = String.fromCharCode(runes[i]);
+
+      _text += char;
+
+      await tester.enterText(_finder, _text);
+    }
+  }
+
+  Future<void> erase() async {
+    await tap();
+
+    while (_text != '') {
+      _text = _text.substring(0, _text.length - 1);
+
+      await tester.enterText(_finder, _text);
+    }
+  }
+
+  Future<void> tap() async {
+    await tester.tap(_finder);
+  }
+}
